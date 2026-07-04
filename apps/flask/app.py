f2590d069a88fdc1f3fa58d149d53188cadd1861
Spas Arnaudov <spasarnaudov@gmail.com>
2026-07-04 17:19:28 +0300

    Load categories from PostgreSQL

--- apps/flask/app.py
+++ apps/flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from db import get_connection
 
 app = Flask(__name__)
 
@@ -10,7 +11,36 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+                SELECT id, name
+                FROM asset_categories
+                ORDER BY id;
+            """)
+            categories = cur.fetchall()
+
+    return render_template("categories.html", categories=categories)
+
+
+@app.route("/assets")
+def assets():
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+                SELECT
+                    assets.id,
+                    assets.symbol,
+                    assets.name,
+                    asset_categories.name AS category_name
+                FROM assets
+                JOIN asset_categories
+                    ON assets.category_id = asset_categories.id
+                ORDER BY assets.id;
+            """)
+            assets = cur.fetchall()
+
+    return render_template("assets.html", assets=assets)
 
 
 if __name__ == "__main__":
